e2a5c04f37ed97791b60c786ba79397e84bc0214
Tania_es <tane.stvs@gmail.com>
2021-03-01 18:42:56 +0100

    curseur+parallax

---
+++ projet-astronommi/code_principal/main.js
@@ -0,0 +1,10 @@
+//Code Javascript pour faire le Parallax
+
+var parallax = document.getElementById("parallax")
+
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset;
+    //console.log('Offset:'+offset)
+    //console.log('offset * 0.7'+ offset * 0.7)
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+})
