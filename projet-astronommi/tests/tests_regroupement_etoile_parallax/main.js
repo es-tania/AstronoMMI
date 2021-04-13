879b0dc588b476b7ca57eb239068454f985a8537
Tania_es <tane.stvs@gmail.com>
2021-04-13 14:26:20 +0200

    test

--- projet-astronommi/tests/tests_regroupement_etoile_parallax/main.js
+++ projet-astronommi/tests/tests_regroupement_etoile_parallax/main.js
@@ -2,9 +2,33 @@
 
 var parallax = document.getElementById("parallax")
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     let offset = window.pageYOffset;
     //console.log('Offset:'+offset)
     //console.log('offset * 0.7'+ offset * 0.7)
     parallax.style.backgroundPositionY = offset * 0.7 + "px";
 })
+
+
+
+
+// Code parallax étoile
+
+document.querySelectorAll(".scene").forEach((elem) => {
+    const modifier = elem.getAttribute("data-modifier");
+
+    basicScroll
+        .create({
+            elem: elem,
+            from: 0,
+            to: 519,
+            direct: true,
+            props: {
+                "--translateY": {
+                    from: "0",
+                    to: `${10 * modifier}px`
+                }
+            }
+        })
+        .start();
+});
