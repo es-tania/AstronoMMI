a5a57833586da7471cebdd9db877c4f99591e02a
Tania_es <tane.stvs@gmail.com>
2021-04-14 13:16:36 +0200

    modif

--- projet-astronommi/code_principal/main.js
+++ projet-astronommi/code_principal/main.js
@@ -6,29 +6,5 @@ window.addEventListener("scroll", function () {
     let offset = window.pageYOffset;
     //console.log('Offset:'+offset)
     //console.log('offset * 0.7'+ offset * 0.7)
-    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+    parallax.style.backgroundPositionY = offset * 0.5 + "px";
 })
-
-
-
-
-// Code parallax étoile
-
-document.querySelectorAll(".scene").forEach((elem) => {
-    const modifier = elem.getAttribute("data-modifier");
-
-    basicScroll
-        .create({
-            elem: elem,
-            from: 0,
-            to: 519,
-            direct: true,
-            props: {
-                "--translateY": {
-                    from: "0",
-                    to: `${10 * modifier}px`
-                }
-            }
-        })
-        .start();
-});
