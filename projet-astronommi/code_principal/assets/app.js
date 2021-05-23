8a4a68f690de06424207e3e420f348fb0d99f0cd
7estelle <layestelle7@gmail.com>
2021-05-23 16:36:21 +0200

    tests validation formaulaire et curseurs

--- projet-astronommi/code_principal/assets/app.js
+++ projet-astronommi/code_principal/assets/app.js
@@ -20,3 +20,33 @@ menuBtn.addEventListener('click', () => {
     menuOpen = false;
   }
 });
+
+
+// Pour le formulaire
+$(document).ready(function(){
+  var nom = null;
+  var prenom = null;
+  var courriel = null;
+$("#nom_utilisateur, #prenom_utilisateur, #courriel").on('keyup',function(){
+       
+  nom=$("#nom_utilisateur").val().length;
+  console.log(nom)
+  if(nom==0){
+      $('#nom_utilisateur').css({"background-color":"#FFC0CB"});
+  }else{
+      $("#nom_utilisateur").css({"background-color":"#B0F2B6"});
+  }
+  prenom=$("#prenom_utilisateur").val().length;
+  if(prenom==0){
+      $('#prenom_utilisateur').css({"background-color":"#FFC0CB"});
+  }else{
+      $("#prenom_utilisateur").css({"background-color":"#B0F2B6"});
+  }
+  courriel=$("#courriel").val().length;
+  if(courriel==0){
+      $('#courriel').css({"background-color":"#FFC0CB"});
+  }else{
+      $("#courriel").css({"background-color":"#B0F2B6"});
+  }
+})
+})
--- projet-astronommi/code_principal/index.php
+++ projet-astronommi/code_principal/index.php
@@ -22,9 +22,12 @@
             <img src="assets/image/planete_footer.png" alt="">
             <form action="envoi.php" method="get">
                 <legend>Pour nous contacter</legend>
-                <input type="text" name="nom_utilisateur" id="nom_utilisateur" placeholder="Nom*"><br>
-                <input type="text" name="prenom_utilisateur" id="prenom_utilisateur" placeholder="Prénom*"><br>
-                <input type="email" name="courriel" id="courriel" placeholder="Email*"><br>
+                <input type="text" name="nom_utilisateur" id="nom_utilisateur" placeholder="Nom*" required="Merci d'entrer votre nom."
+                data-validation-required-message="Merci d'entrer votre nom."><br>
+                <input type="text" name="prenom_utilisateur" id="prenom_utilisateur" placeholder="Prénom*" required=""
+                data-validation-required-message="Merci d'entrer votre prénom."><br>
+                <input type="email" name="courriel" id="courriel" placeholder="Email*" required=""
+                data-validation-required-message="Merci d'entrer votre adresse de courriel."><br>
                 <textarea rows="5" cols="50" name="message" placeholder="Message"></textarea><br>
                 <input type="submit" id="envoi" value="Envoyer">
             </form>
@@ -35,6 +38,7 @@
 </body>
 <script type="text/javascript" src="assets/app.js"></script>
 <script type="text/javascript" src="autres_pages/page_chargement/app.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+    </script>
 
 </html>
