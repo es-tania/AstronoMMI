87ff502347b63dd1d831a12f114cb89ed8313904
Tania_es <tane.stvs@gmail.com>
2021-05-31 22:04:44 +0200

    Update app.js

--- projet-astronommi/code_principal/assets/app.js
+++ projet-astronommi/code_principal/assets/app.js
@@ -1,11 +1,18 @@
 //Code Javascript pour faire le Parallax
-var parallax = document.getElementById("parallax")
+// var parallax = document.getElementById("parallax")
 
-window.addEventListener("scroll", function () {
-    let offset = window.pageYOffset;
-    //console.log('Offset:'+offset)
-    //console.log('offset * 0.7'+ offset * 0.7)
-    parallax.style.backgroundPositionY = offset * 0.5 + "px";
+// window.addEventListener("scroll", function () {
+//     let offset = window.pageYOffset;
+//     //console.log('Offset:'+offset)
+//     //console.log('offset * 0.7'+ offset * 0.7)
+//     parallax.style.backgroundPositionY = offset * 0.5 + "px";
+// })
+
+var parallax = $('#parallax');
+
+$(window).scroll(function(){
+    let offset = $(window).scrollTop();
+    $(parallax).css('background-position-y' , offset * 0.5 +'px');
 })
 
 // Animation bouton du menu
